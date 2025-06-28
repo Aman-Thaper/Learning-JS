8aac03751f399ba0e42ddfcfd7263c335a2f5fda
Aman-Thaper <work.aman04@gmail.com>
2025-06-29 02:37:11 +0530

    datatype conversion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using node not browser
+
+console.log(3+3*2);
+// code readability should be high
+
+let name = "aman"
+let age = 21
+let loggedIn = true
+
+// string , int , boolean ,bigint(number >= 2^53)
+// null -> standalone value
+//undefined
+//symbol -. unique
+//object
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof loggedIn);
+console.log(typeof undefined);//undefined
+console.log(typeof null)//object
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score = "44a"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+//"33"->33
+//"3eda"->NaN
+// true -> 1 | false -> 0 and vice versa
+
+let isLoggedin=1
+
+console.log(Boolean(isLoggedin));  // 1-> true 
