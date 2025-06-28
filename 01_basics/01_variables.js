645640183220ee58936894ea5ceb1e4ed3f61ce3
Aman-Thaper <work.aman04@gmail.com>
2025-06-28 23:50:44 +0530

    Commiting from VS code editor

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const account_ID = 14424
+let account_Email = "aman@whatver.com"
+var account_password = "1234"
+accountCity = "Jaipur"
+let account_State
+
+//account_ID = 2
+account_Email="bruh@re"
+account_password="32"
+accountCity="Mumbai"
+
+console.table([account_ID,account_Email,account_password,accountCity,account_State])
+
+/*
+Prefer not using var
+because of scope issues
+*/
