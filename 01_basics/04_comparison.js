ca39cf094294865b57527685703bf8a81533b237
Aman-Thaper <work.aman04@gmail.com>
2025-06-29 03:42:31 +0530

    comparison operator and strict check

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,12 @@
+console.log("0.20">1);
+console.log(null > 0);
+console.log(null >= 0);//true huh??
+console.log(null == 0);
+
+console.log(undefined > 0);
+console.log(undefined >= 0);
+console.log(undefined == 0);
+
+// === operator strict check
+
+console.log("2"===2);
