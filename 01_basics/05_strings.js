641c9e891eb66c627a9231455be134f2aa63554a
Aman-Thaper <work.aman04@gmail.com>
2025-06-30 10:42:32 +0530

    Added Strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "Aman"
+const age = 21
+
+
+// ``->used for string formatting/ interpolation, along with ${variable} syntax
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
+
+const state = new String('Maharashtra')
+console.log(state[0]);
