b53287a3c667d52bba547f0247f03c275a3ceae7
Aman-Thaper <work.aman04@gmail.com>
2025-07-01 12:27:21 +0530

    Completed basic strings

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -8,3 +8,19 @@ console.log(`Hello my name is ${name} and my age is ${age}`);
 
 const state = new String('Maharashtra')
 console.log(state[0]);
+
+console.log(state.substring(0,11))
+
+const v = new String("       A man    ")
+console.log(v.trim());
+
+console.log(state.replace('Mah','Dug'));
+console.log(state.includes('rashtra'))
+
+let arr = new Array()
+for (let i = 0; i < state.length; i++) {
+    arr[i]=state[i];
+}
+
+console.log(arr);
+console.log(state.split(""));
